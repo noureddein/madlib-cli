f6941c699f6dfb58b842c43a0bc4b36f21b2949d
noureddein <noureddein@gmail.com>
2022-03-07 22:28:19 +0300

    add a discription for each function add descriptions for each function, and describe the project in README file.

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -23,12 +23,27 @@ welcome_msg = """
 
 
 def get_fields(text):
+    """
+        This function catch all the tempelates fields in the text.
+        Input:
+            String --> Text
+
+        Output:
+            Tuple --> Return all tempelates fields.
+    """
     pattern = r'{([^}]+)}'
     match = tuple(re.findall(pattern, text))
     return match
 
 
 def get_user_inputs(fields):
+    """
+        The function ask the user for inputs, depending on the tempelates fields.
+        Input:
+            Tuple --> Tuple containing fields tempelates.
+        Output:
+            list --> Return list of the user inputs.
+    """
     user_inputs = []
     for field in fields:
         user_answer = input(f'Enter a {field}: ')
@@ -39,29 +54,55 @@ def get_user_inputs(fields):
 
 
 def read_template(path):
+    """
+        Function take a path of a file, and return it's containt.
+        Input:
+            String --> Path of the file as a string.
+        Output:
+            String --> Return file content.
+    """
     try:
         with open(f'../{path}', 'r') as f:
             content = f.read()
     except:
-        raise FileNotFoundError('Check the path of the file!')
+        raise FileNotFoundError('Check file path!')
     else:
         return content
 
 
 def parse_template(text):
+    """
+        The function replace all the tempelates fields with empty curly braces,
+        to prepare the text for filling of user inputs.
+        Input:
+            String --> The text of tempelates fields
+        Output:
+            String --> Return The new prepared text.
+            Tuple --> Return all tempelates fields.
+
+    """
     fields = get_fields(text)
     pattern = r'{([^}]+)}'
     new_text = re.sub(pattern, "{}", text)
     return new_text, fields
 
 
-def merge(text, user_inputs):
-    return text.format(*user_inputs)
+def merge(new_text, user_inputs):
+    """
+        The function fill all user inputs into the text.
+        and return a new text.
+
+        Input:
+            String --> Text with empty tempelates fields.
+            Tuple -->  User inputs.
+    """
+    return new_text.format(*user_inputs)
 
 
-# print(welcome_msg)
-# text = read_template('assets/sample.txt')
-# new_text, user_inputs = parse_template(text)
-# user_inputs = tuple(get_user_inputs(fields))
-# print('------------------------------ Here are your answers ------------------------')
-# print(merge(new_text, user_inputs))
+print(welcome_msg)
+text = read_template('assets/sample.txt')
+new_text, user_inputs = parse_template(text)
+fields = get_fields(text)
+user_inputs = tuple(get_user_inputs(fields))
+print('------------------------------ Here are your answers ------------------------')
+print(merge(new_text, user_inputs))
